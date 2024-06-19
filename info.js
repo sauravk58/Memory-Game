54ba4e2468473d2780a5bf7aaa9497613dff59e4
sauravk58 <144691979+sauravk58@users.noreply.github.com>
2024-06-19 13:53:18 +0530

    Add files via upload

---
+++ info.js
@@ -0,0 +1,6 @@
+/*
+1)first we have to define three stage for play,game board ,and win. for that we are using a useState
+hook in react js and set the initial state as an empty array.
+2)In gameBoard function we will create a memory game board with cards face down by mapping through our card data
+
+*/
